ff0e33437b663d7e7f022d8f41b4946a9d643860
Francis Loyd Raval <jasmarket577@gmail.com>
2025-03-22 09:31:24 +0000

    im okay im fine

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cai = require("./routes/cai");
 const ai = require("./routes/ai");
+
 const app = express();
 
 app.use(express.json());
 app.use("", ai);
+app.use("", cai);
 
 async function main(){
   app.listen(8080)
